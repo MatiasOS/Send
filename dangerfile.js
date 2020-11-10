879e9b78e63e4e9ce90e7f6704024dde16cb06a1
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2020-11-10 08:55:53 -0300

    ci(danger): rules

--- dangerfile.js
+++ dangerfile.js
@@ -1,4 +1,16 @@
-const {message, danger} = require("danger");
+const {message, danger, warn} = require("danger");
 
-const modifiedMD = danger.git.modified_files.join("- ")
-message("Changed Files in this PR: \n - " + modifiedMD)
+const modifiedMD = danger.git.modified_files.join("- ");
+message("Changed Files in this PR: \n - " + modifiedMD);
+
+if (!danger.github.pr.assignees.length) {
+  warn("No te olvides de asignar a alquien a este PR!");
+}
+
+if (!danger.github.pr.assignees.length) {
+  warn("No te olvides de asignar a alquien a este PR!");
+}
+
+if(danger.github.requested_reviewers.length < 1) {
+  warn("No te olvides de seleccionar al menos 1 reviewer a este PR!");
+}
