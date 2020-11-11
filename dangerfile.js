76375d1e7087dc9345daec26c5fb7c6679569a24
Matias Oveja Smith <matias.ovejasmith@gmail.com>
2020-11-11 09:04:10 -0300

    refactor(danger): move things up and down

--- dangerfile.js
+++ dangerfile.js
@@ -1,22 +1,6 @@
 const {message, danger, warn, fail} = require("danger");
 const { basename } = require('path');
 
-
-const modifiedMD = danger.git.modified_files.join("- ");
-message("Changed Files in this PR: \n - " + modifiedMD);
-
-if (!danger.github.pr.assignees.length) {
-  warn("No te olvides de asignar a alquien a este PR!");
-}
-
-if(danger.github.requested_reviewers.length < 1) {
-  warn("No te olvides de seleccionar al menos 1 reviewer a este PR!");
-}
-
-if (danger.github.pr.body.length < 10) {
-  fail("Agregar una descripción al PR!");
-}
-
 const totalModifications = async () => {
   const { data: fileList } = await danger.github.api.pulls.listFiles({
     owner: danger.github.thisPR.owner,
@@ -37,21 +21,33 @@ const totalModifications = async () => {
     message(`Este PR tiene un tamño ideal! Solo ${additions + deletions} modificaciones!`);
   }
 
-  if (additions > 200 && additions < 300) {
-    warn(`Límite de **additions** es 300. Actualmente estás en ${additions}`);
+  if (additions > 250 && additions < 300) {
+    warn(`Te estás acercandp al límite de 300 **additions**! Actualmente estás en ${additions}`);
   }
 
   if (additions > 300) {
-    fail(`Límite de **additions** es 300. Actualmente estás en ${additions}`);
+    fail(`Superaste el límite de 300 **additions**. Actualmente estás en ${additions}`);
   }
 
-  if (deletions > 200 && deletions < 300) {
-    warn(`Límite de **deletions** es 300. Actualmente estás en ${deletions}`);
+  if (deletions > 250 && deletions < 300) {
+    warn(`Te estás acercandp al límite de 300 **deletions**! Actualmente estás en ${deletions}`);
   }
 
   if (deletions > 300) {
-    fail(`Límite de **deletions** es 300. Actualmente estás en ${deletions}`);
+    fail(`Superaste el límite de 300 **deletions**. Actualmente estás en ${deletions}`);
   }
 };
+
+if (!danger.github.pr.assignees.length) {
+  warn("No te olvides de asignar a alquien a este PR!");
+}
+
+if(danger.github.requested_reviewers.length < 1) {
+  warn("No te olvides de seleccionar al menos 1 reviewer a este PR!");
+}
+
+if (danger.github.pr.body.length < 10) {
+  fail("Por favor, agregar una descripción al PR.");
+}
 
 totalModifications();
